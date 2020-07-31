b361c07b9b5c337b644688c28190f64ce9dd574e
John Ewart <john@johnewart.net>
2020-07-31 11:08:38 -0700

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("A change");
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello happy world!" );
     }
 }
