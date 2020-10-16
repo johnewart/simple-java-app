33ac76d37fd2b591ad2b8bdbff53e7560c6aa75f
John Ewart <john@johnewart.net>
2020-10-16 07:52:44 -0700

    Small update

---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,30 @@
+package com.mycompany.app;
+
+import com.mycompany.app.App; 
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    @Test
+    public void testAnotherThing() { 
+        System.out.println("Testing");
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+   
+    }
+    
+    @Test
+    public void testTheApp() {
+        String[] args = { "hello", "world" };
+        App.main(args);
+    }
+}
